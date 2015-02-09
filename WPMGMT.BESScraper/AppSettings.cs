88b3194e6d5976ba5e3604bfbc6d3aa330e13a95
TheGameSquid <robin.mattheussen@gmail.com>
2015-02-09 13:45:22 +0100

    Started working on scraper

---
+++ WPMGMT.BESScraper/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using System.ComponentModel;
+
+namespace WPMGMT.BESScraper
+{
+    public static class AppSettings
+    {
+        public static T Get<T>(string key)
+        {
+            var appSetting = ConfigurationManager.AppSettings[key];
+            if (String.IsNullOrWhiteSpace(appSetting))
+            {
+                throw new Exception(String.Format("Key {0} was not found", key));
+            }
+
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            return (T)(converter.ConvertFromInvariantString(appSetting));
+        }
+    }
+}
--- WPMGMT.BESScraper/Program.cs
+++ WPMGMT.BESScraper/Program.cs
@@ -11,31 +11,8 @@ namespace WPMGMT.BESScraper
     {
         static void Main(string[] args)
         {
-            // Use to ignore SSL errors
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
-
-            var client = new RestClient("https://pc120006933:52311/api/");
-            client.Authenticator = new HttpBasicAuthenticator("iemadmin", "bigfix");
-
-            RestRequest request = new RestRequest("actions", Method.GET);
-            //request.AddParameter("actions", "");
-
-            // Execute the request
-            IRestResponse<List<WPMGMT.BESScraper.Model.Action>> response = client.Execute<List<WPMGMT.BESScraper.Model.Action>>(request);
-
-            try
-            {
-                // If the response contains an Exception
-                if (response.ErrorException != null)
-                {
-                    // Throw it back up
-                    throw response.ErrorException;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception encountered: {0}", ex.Message);
-            }
+            Scraper scraper = new Scraper("https://pc120006933:52311/api/", "iemadmin", "bigfix");
+            List<WPMGMT.BESScraper.Model.Action> actions = scraper.GetActions();
 
             Console.Read();
         }
